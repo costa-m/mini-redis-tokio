1ca5b48cd9ab1ea7d0a859f86338d3e6996e124b
pont-os <pontos@mm.st>
2022-08-24 22:26:18 -0300

    parse_frame method for Connection

--- src/connection.rs
+++ src/connection.rs
@@ -2,6 +2,7 @@ use bytes::Buf;
 use bytes::BytesMut;
 use mini_redis::frame::Error::Incomplete;
 use mini_redis::{Frame, Result};
+use std::io::Cursor;
 use tokio::io::AsyncReadExt;
 use tokio::net::TcpStream;
 
@@ -50,4 +51,33 @@ impl Connection {
 
     // Write a frame to the connection.
     pub async fn write_frame(&mut self, frame: &Frame) -> Result<()> {}
+
+    fn parse_frame(&mut self) -> Result<Option<Frame>> {
+        // Create the `T: Buf` type.
+        let mut buf = Cursor::new(&self.buffer[..]);
+
+        // Check whether a full frame is available
+        match Frame::check(&mut buf) {
+            Ok(_) => {
+                // Get the byte length of the frame
+                let len = buf.position() as usize;
+
+                // Reset the internal cursor for the call to `parse`
+                buf.set_position(0);
+
+                // Parse the frame
+                let frame = Frame::parse(&mut buf)?;
+
+                // Discard the frame from the buffer
+                self.buffer.advance(len);
+
+                // Return the frame to the caller.
+                Ok(Some(frame))
+            }
+            // Not enough data has been buffered
+            Err(Incomplete) => Ok(None),
+            // An error was encountered
+            Err(e) => Err(e.into()),
+        }
+    }
 }
